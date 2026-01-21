807c52e7d273358668c2952296a875c99e7b8761
Fred Hsu <fredlhsu@gmail.com>
2026-01-21 10:44:05 -0800

    week 2 session 2

---
+++ q1/week2-README.md
@@ -0,0 +1,17 @@
+# Week 2 Session
+
+## What works
+
+- The overall functionality works, and is verified with the numerical gradient calculation.
+- There are abstractions for the various layers and neurons that are functional.
+- The XOR training mostly works
+
+## What's hacky
+
+- Keeping track of the parameters and manual creation of the Value objects
+
+## What I'll improve
+
+- Better debugging output
+- Number coercion
+- Clean up based on code review
---
+++ q1/week2.py
@@ -0,0 +1,359 @@
+from collections.abc import Callable
+import math
+import random
+from typing import Self, override
+
+
+# Week 2 Session 2:
+# Add a very thin Vector/Tensor convenience layer (array of Values) for dot products.
+# Ensure you have: add, mul, pow, neg/sub, div, exp, log, tanh or sigmoid, relu
+# Build MLP module
+#   Neuron, Layer, MLP
+# Loss and training loop
+#  XOR dataset
+#  Loss : MSE
+#  SGD with fixed LR
+# Finite-difference gradient check on one param (weight)
+
+
+class Value:
+    data: float
+    grad: float
+    label: str
+    _prev: list[Self]
+    _backward: Callable[..., None]
+
+    def __init__(self, data: float, prev: list[Self], label: str):
+        self.data = data
+        self.grad = 0.0
+        self._prev = prev
+        self._backward = lambda: None
+        self.label = label
+
+    @override
+    def __repr__(self):
+        return f"label: {self.label} data: {self.data}, grad: {self.grad}, prev: {self._prev}"
+
+    def __add__(self, other: Self):
+        out = Value(data=self.data + other.data, prev=[self, other], label="+")
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other: Self):
+        out = Value(data=self.data * other.data, prev=[self, other], label="*")
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __div__(self, other: Self):
+        return self * other**-1
+
+    def __pow__(self, other: int):
+        out = Value(data=self.data**other, prev=[self], label="^")
+
+        def _backward():
+            self.grad += other * self.data ** (other - 1) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __exp__(self):
+        out = Value(data=math.exp(self.data), prev=[self], label="exp")
+
+        def _backward():
+            self.grad += math.exp(self.data) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __log__(self):
+        out = Value(data=math.log(self.data), prev=[self], label="log")
+
+        def _backward():
+            self.grad += self.data**-1 * out.grad
+
+        out._backward = _backward
+        return out
+
+    def relu(self):
+        out = self.data if self.data > 0 else 0
+        out = Value(out, [self], label="relu")
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def sigmoid(self):
+        data = data = 1.0 / (1.0 + math.exp(-self.data))
+        out = Value(data, prev=[self], label="sigmoid")
+
+        def _backward():
+            self.grad += data * (1 - data) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def backward(self):
+        topo: list[Value] = []
+        visited: set[Value] = set()
+
+        def build_graph(v: Value):
+            if v not in visited:
+                visited.add(v)
+                for p in v._prev:
+                    build_graph(p)
+                topo.append(v)
+
+        build_graph(self)
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward()
+
+    # week 2 session 1: 12-JAN
+    def square(self):
+        out = self.data**2
+        out = Value(out, [self], label="square")
+
+        def _backward():
+            self.grad += 2 * self.data * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __neg__(self):
+        return self * Value(-1, [], label="neg")
+
+    def __sub__(self, other: Self):
+        out = self.data - other.data
+        out = Value(out, [self, other], label="-")
+
+        def _backward():
+            self.grad += out.grad
+            other.grad -= out.grad  # Fixed: subtraction gradient
+
+        out._backward = _backward
+        return out
+
+    def __radd__(self, other: Self):
+        return other + self
+
+    def __iadd__(self, other: Self):
+        return self + self + other
+
+
+type Vector = list[Value]
+
+
+class Neuron:
+    weights: Vector
+    bias: Value
+
+    def __init__(self, weights: Vector, bias: float) -> None:
+        self.weights = weights
+        self.bias = Value(bias, [], "b")
+
+    def __call__(self, inputs: Vector, non_lin: str = "relu"):
+        weighted = [x * w for (x, w) in zip(inputs, self.weights)]
+        out = self.bias
+        for n in weighted:
+            out = out + n
+        if non_lin == "sigmoid":
+            return out.sigmoid()
+
+        return out.relu()
+
+    @override
+    def __repr__(self):
+        return f"Neuron({len(self.weights)})"
+
+    def zero_gradients(self):
+        for w in self.weights:
+            w.grad = 0.0
+        self.bias.grad = 0.0
+
+    def parameters(self):
+        return self.weights + [self.bias]
+
+
+def default_neuron(nin: int, label: str = "w"):
+    weights = [Value(random.uniform(-1, 1), [], label + str(i)) for i in range(nin)]
+    bias = 0.0
+    return Neuron(weights, bias)
+
+
+class Layer:
+    neurons: list[Neuron]
+    non_lin: str
+
+    def __init__(self, neurons: list[Neuron], non_lin: str = "relu") -> None:
+        self.neurons = neurons
+        self.non_lin = non_lin
+
+    def __call__(self, x: Vector):
+        out = [n(x, self.non_lin) for n in self.neurons]
+        return out
+
+    @override
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+
+    def zero_gradients(self):
+        for n in self.neurons:
+            n.zero_gradients()
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+
+
+def create_layer(nin: int, nout: int, non_lin: str = "relu"):
+    neurons = [default_neuron(nin, "w" + str(i)) for i in range(nout)]
+    return Layer(neurons, non_lin=non_lin)
+
+
+class MLP:
+    layers: list[Layer]
+
+    def __init__(self, layers: list[Layer]):
+        self.layers = layers  # [::-1]
+
+    def __call__(self, x: Vector):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+def numerical_grad():
+    layers = [create_layer(2, 4), create_layer(4, 1, non_lin="sigmoid")]
+    mlp = MLP(layers)
+
+    eps = 1e-4
+    input_data = [0, 1]
+    target = 1
+
+    # First forward pass to get analytical gradient
+    xs = [Value(x, [], "x") for x in input_data]
+    out = mlp(xs)
+    loss = (out[0] - Value(target, [], "y")).square()
+    loss.backward()
+
+    # Store the analytical gradient and weight value
+    weight_to_check = mlp.layers[0].parameters()[4]
+    analytical_gradient = weight_to_check.grad
+    original_weight_value = weight_to_check.data
+
+    print(f"Weight: {weight_to_check}")
+    print(f"Analytical gradient: {analytical_gradient}")
+
+    # Perturb weight up
+    weight_to_check.data = original_weight_value + eps
+    xs_plus = [Value(x, [], "x") for x in input_data]  # Fresh values
+    out_plus = mlp(xs_plus)
+    loss_plus = (out_plus[0] - Value(target, [], "y")).square()
+
+    # Perturb weight down
+    weight_to_check.data = original_weight_value - eps
+    xs_minus = [Value(x, [], "x") for x in input_data]  # Fresh values
+    out_minus = mlp(xs_minus)
+    loss_minus = (out_minus[0] - Value(target, [], "y")).square()
+
+    # Restore original weight
+    weight_to_check.data = original_weight_value
+
+    # Compute numerical gradient using centered difference
+    numerical_gradient = (loss_plus.data - loss_minus.data) / (2 * eps)
+
+    print(f"Numerical gradient: {numerical_gradient}")
+    print(f"Difference: {abs(analytical_gradient - numerical_gradient)}")
+    print(f"Match: {abs(analytical_gradient - numerical_gradient) < 1e-5}")
+
+
+def xor():
+    print("\nxor")
+    xss = [[0, 0], [0, 1], [1, 1], [1, 0]]
+    yss = [0, 1, 0, 1]
+    learning_rate = 0.1
+    layers = [create_layer(2, 8), create_layer(8, 1, non_lin="sigmoid")]
+    mlp = MLP(layers)
+    print(mlp.layers)
+    for k in range(200):
+        print(f"iteration: {k}")
+        for xs, ys in zip(xss, yss):
+            xs = [Value(x, [], "x") for x in xs]
+            # ys = [[0], [1], [0], [1]]
+            out = mlp(xs)
+            loss = (out[0] - Value(ys, [], "y")).square()  # loss is MSE
+            print(f"\nloss: {loss.data}")
+            # zero all gradients
+            for layer in layers:
+                layer.zero_gradients()
+            loss.backward()
+            # update
+            for layer in layers:
+                for p in layer.parameters():
+                    # print(f"previous: {p.data}")
+                    p.data -= p.grad * learning_rate
+                    # print(f"updated: {p.data}")
+
+
+def two_layer_simple():
+    xs = [Value(x, [], "x") for x in [1.0, 2.0, 3.0]]
+    w1 = [Value(w, [], "w") for w in [2.0, 4.0, 6.0]]
+
+    bias = 5.0
+    neuron = Neuron(weights=w1, bias=bias)
+    print(neuron(xs))
+    n2 = default_neuron(3)
+    print(n2)
+    for n in n2.weights:
+        print(n)
+
+    # Testing with simple two layer
+    # x = 2, y = 2
+    x = Value(data=2, prev=[], label="x")
+    y = [Value(data=2.0, prev=[], label="y")]
+    z1 = Layer([Neuron([Value(0.1, [], label="w1")], 0.1)])
+    # a1 = ReLU(z1) = .1*x+.1 = .3
+    a1 = z1([x])
+    print(f"a1: {a1}")
+    # z2 = W2*a1 + b2 = .5*a1 + .1 = .25
+    z2 = Layer([Neuron([Value(0.5, [], label="w2")], 0.1)], non_lin="sigmoid")
+    # y_hat=sigmoid(z2) = .562
+    y_hat = z2(a1)
+    print(f"\ny_hat: {y_hat[0]}")
+    # loss = (y_hat-y)^2 = (.562 - 2)^2 = 2.067
+    loss = (y_hat[0] - y[0]) ** 2
+
+    # print(y_hat)
+    print(loss)
+    loss.backward()
+    print()
+    print(loss)
+    # Print the w2 values, gradient should be \frac{\partial L}{\partial w_2}=(\hat{y}-y)\hat{y}(1-\hat{y})a_1 * 2 = -0.212
+    # w2 happens to be the last element of the z2 layer
+    print("\nw2.grad should be ~ -0.212")
+    print(z2.neurons[-1].weights)
+
+
+def main():
+    print("week 2")
+    random.seed(42)
+
+    xor()
+
+    numerical_grad()
+
+
+if __name__ == "__main__":
+    main()
